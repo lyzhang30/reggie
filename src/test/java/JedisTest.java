61b5431dc397e86db3f93710557e32c83995e735
zhanglianyong <20lyzhang1@stu.edu.cn>
2022-08-25 16:43:17 +0800

    Redis的测试

--- src/test/java/JedisTest.java
+++ src/test/java/JedisTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 //import redis.clients.jedis.Jedis;
 
 import java.util.Set;
@@ -22,35 +23,35 @@ public class JedisTest {
     //    redisTemplate.opsForCluster().set("city","beijing");
     //}
 
-    //@Test
-    //public void testRedis() {
-    //    //连接
-    //    Jedis jedis = new Jedis("localhost", 6379);
-    //
-    //    //执行具体的操作
-    //    jedis.set("username","zhanglianyong");
-    //
-    //    String name = jedis.get("username");
-    //    System.out.println(name);
-    //
-    //    //删除
-    //    jedis.del("username");
-    //    jedis.set("username","niuren");
-    //
-    //    jedis.hset("myhash","name","张连勇");
-    //    String myName = jedis.hget("myhash", "name");
-    //    System.out.println(myName);
-    //
-    //    //获取所有的key
-    //    Set<String> keys = jedis.keys("*");
-    //    for(String key : keys){
-    //        System.out.println(key);
-    //    }
-    //    //关闭连接
-    //    jedis.close();
-    //
-    //
-    //
-    //}
+    @Test
+    public void testRedis() {
+        //连接
+        Jedis jedis = new Jedis("localhost", 6379);
+
+        //执行具体的操作
+        jedis.set("username","zhanglianyong");
+
+        String name = jedis.get("username");
+        System.out.println(name);
+
+        //删除
+        jedis.del("username");
+        jedis.set("username","niuren");
+
+        jedis.hset("myhash","name","张连勇");
+        String myName = jedis.hget("myhash", "name");
+        System.out.println(myName);
+
+        //获取所有的key
+        Set<String> keys = jedis.keys("*");
+        for(String key : keys){
+            System.out.println(key);
+        }
+        //关闭连接
+        jedis.close();
+
+
+
+    }
 
 }
---
+++ src/test/java/RedisTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+
+/**
+ * Redis测试类
+ * @author zhanglianyong
+ * 2022/8/2311:36
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RedisTest {
+
+    @Test
+    public void test() {
+        Jedis jedis = new Jedis("localhost", 6379);
+        jedis.set("zhanglianyong", "21");
+        System.out.println(jedis.get("zhanglianyong"));
+    }
+}
